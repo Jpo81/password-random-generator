9cffd860ea26681b0854960d72149879dcea29c4
Miguel Bonilha <126339796+Jpo81@users.noreply.github.com>
2024-07-20 09:52:10 -0300

    Update senha.js

--- senha.js
+++ senha.js
@@ -6,6 +6,7 @@ function gerarSenha() {
         password += charset.charAt(Math.floor(Math.random() * n));
     }
     document.getElementById('senha-gerada').innerText = password;
+    document.getElementById('btn-copiar').style.display = 'block';
 }
 
 function gerarSenhaSimples() {
@@ -16,9 +17,25 @@ function gerarSenhaSimples() {
         password += charset.charAt(Math.floor(Math.random() * n));
     }
     document.getElementById('senha-gerada').innerText = password;
+    document.getElementById('btn-copiar').style.display = 'block';
 }
 
 function updateLengthDisplay() {
     const length = document.getElementById('senha-slider').value;
     document.getElementById('senha-length').innerText = length;
+}
+
+function copiarSenha() {
+    const senhaGerada = document.getElementById('senha-gerada').innerText;
+    navigator.clipboard.writeText(senhaGerada).then(function() {
+        alert('Senha copiada para a área de transferência!');
+    }, function(err) {
+        console.error('Erro ao copiar a senha: ', err);
+    });
+}
+
+function toggleTheme() {
+    const body = document.body;
+    body.classList.toggle('dark-theme');
+    body.classList.toggle('light-theme');
 }
